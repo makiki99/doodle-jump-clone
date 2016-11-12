ea5cdeb34731a5697919a8fd133327fe74c86f44
makiki99 <mat.gawro@gmail.com>
2016-11-12 15:19:54 +0100

    done evne more stuff

--- code/main.js
+++ code/main.js
@@ -1,6 +1,8 @@
 var game = new Phaser.Game(480, 640, Phaser.AUTO, '');
 
 game.state.add('play', {
+	self: this,
+
 	preload: function(){
 		//assets
 		game.load.image("player", "assets/player.png");
@@ -9,10 +11,11 @@ game.state.add('play', {
 	},
 
 	create: function(){
+		//creates proper bounduaries
 		//starts physics
 		game.physics.startSystem(Phaser.Physics.ARCADE)
 		//background
-		game.add.sprite(0,0,"bg");
+		this.background = game.add.sprite(0,0,"bg");
 		//player
 		this.player = game.add.sprite(220,360,"player");
 		game.physics.arcade.enable(this.player);
@@ -23,12 +26,12 @@ game.state.add('play', {
 		for (let i = 0; i < 128; i++) {
 			// create buffer of platforms, because constantly deleting and creating
 			// objects is slow
-			this.platforms.create(Math.random()*480,Math.random()*640,"platform");
+			this.platforms.create(Math.random()*387,i*(64),"platform");
 		}
 		this.platforms.children.forEach(
 			// assigns proper properties to all platforms
 			i => {
-				i.exists = false;
+				i.exists = true;
 				i.body.immovable = true;
 				// properties below are needed to make jumping from below possible
 				i.body.checkCollision.down = false;
@@ -36,9 +39,9 @@ game.state.add('play', {
 				i.body.checkCollision.right = false;
 			}
 		)
-		for (var i = 0; i < 6; i++) {
+		for (var i = 122; i < 128; i++) {
 			// sets up floor
-			this.platforms.children[i].x = i*93-41;
+			this.platforms.children[i].x = (i-122)*93-41;
 			this.platforms.children[i].y = 630;
 			this.platforms.children[i].exists = true;
 		}
@@ -51,6 +54,16 @@ game.state.add('play', {
 			// when collision occurs, perform a jump
 			this.player.body.velocity.y = -1050;
 		}
+		game.camera.y = this.player.y-320
+		game.world.wrap(this.player,0,true,true,false);
+		this.player.body.velocity.x = 0;
+		if (true) {
+			this.player.body.velocity.x += 250;
+		}
+		//move world bounduaries to create illusion of infinite world
+		game.world.setBounds(0, -320+this.player.y, 480, 640);
+		//move background
+		this.background.y = game.world.y;
 	}
 });
 
